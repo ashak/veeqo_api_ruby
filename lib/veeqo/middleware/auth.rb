5dd45d2bf01de5d53cc621cef6e35dab88debfdd
Gary Richards <garyr@opensourcery.uk>
2023-04-04 19:31:44 +0100

    Support oauth2

--- lib/veeqo/middleware/auth.rb
+++ lib/veeqo/middleware/auth.rb
@@ -7,7 +7,8 @@ module Veeqo
       end
 
       def call(env)
-        env[:request_headers]['x-api-key'] = @options[:api_key]
+        env[:request_headers]['Authorization'] = "Bearer #{@options[:token]}" if @options[:token]
+        env[:request_headers]['x-api-key'] = @options[:api_key] if @options[:api_key]
         env[:request_headers]['x-api-request'] = 'true'
         @app.call env
       end
